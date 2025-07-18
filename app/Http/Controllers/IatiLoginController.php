6532b30fa2eb3a998127a40bb12a78fa32aa1866
PG-Momik <momik12339@gmail.com>
2025-07-18 15:25:20 +0545

    - [x] Added 'jumbojett/openid-connect-php' OIDC client package

---
+++ app/Http/Controllers/IatiLoginController.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Session;
+use Jumbojett\OpenIDConnectClient;
+
+class IatiLoginController extends Controller
+{
+    /**
+     * @return OpenIDConnectClient
+     */
+    private function getClient(): OpenIDConnectClient
+    {
+        $config = config('services.oidc');
+
+        $oidc = new OpenIDConnectClient(
+            provider_url : rtrim($config['issuer'], '/'),
+            client_id    : $config['client_id'],
+            client_secret: $config['client_secret']
+        );
+
+        $oidc->setRedirectURL($config['redirect_uri']);
+        $oidc->addScope(['openid', 'email', 'profile']);
+
+        return $oidc;
+    }
+
+    /**
+     * @throws \Jumbojett\OpenIDConnectClientException
+     */
+    public function redirectToProvider(): void
+    {
+        $this->getClient()->authenticate();
+    }
+
+    public function handleProviderCallback(): JsonResponse|RedirectResponse
+    {
+        try {
+            $oidc = $this->getClient();
+            $oidc->authenticate();
+            $userInfo = $oidc->requestUserInfo();
+
+            Session::put('user', [
+                'name' => $userInfo->name ?? $userInfo->preferred_username ?? 'Unknown',
+                'email' => $userInfo->email ?? null,
+            ]);
+
+            return redirect()->intended('/');
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * @return Redirector|Application|RedirectResponse
+     */
+    public function logout(): Redirector|Application|RedirectResponse
+    {
+        Session::flush();
+
+        $logoutUrl = config('services.oidc.logout_endpoint') . '?' . http_build_query([
+                'post_logout_redirect_uri' => config('services.oidc.logout_redirect_uri'),
+            ]);
+
+        return redirect($logoutUrl);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\Activity\ActivityController;
+use App\Http\Controllers\IatiLoginController;
 use App\Http\Middleware\RedirectIfAuthenticated;
 use Illuminate\Support\Facades\Route;
 
@@ -52,3 +53,8 @@ Route::get('/duplicate-activity', [ActivityController::class, 'duplicateActivity
 Route::get('/language/{language}', [App\Http\Controllers\Web\WebController::class, 'setLocale'])->name('set-locale');
 Route::get('/current-language', [App\Http\Controllers\Web\WebController::class, 'getLocale'])->name('get-locale');
 Route::get('/translated-data', [App\Http\Controllers\Web\WebController::class, 'getTranslatedData'])->name('get-translated-data');
+
+Route::get('/login/iati', [IatiLoginController::class, 'redirectToProvider'])->name('login.iati');
+Route::get('/login/iati/callback', [IatiLoginController::class, 'handleProviderCallback'])->name('login.iati.callback');
+Route::get('/logout/iati', [IatiLoginController::class, 'logout'])->name('logout.iati');
+Route::get('/logout/callback', fn () =>  redirect('/'))->name('logout.iati.callback');
