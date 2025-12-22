958f6685b9d094cde8fd1f2ff68ad83586b6f620
Ashesh Shakya <ashesh112233@gmail.com>
2025-12-22 15:27:23 +0545

    fixed iati_superadmin flow

--- app/IATI/Services/RegisterYourDataApi/IatiDataSyncService.php
+++ app/IATI/Services/RegisterYourDataApi/IatiDataSyncService.php
@@ -179,6 +179,7 @@ class IatiDataSyncService
     {
         return match ($registryRole) {
             'provider_admin' => 'iati_admin',
+            'iati_superadmin' => 'iati_admin',
 //            'admin'          => 'admin',
 //            'editor'         => 'admin',
 //            'contributor'    => 'admin',
